74324fb1e6311940c24abfa96cfc01c577b1d80d
Kirill Tikhanskiy <s8ckerpunch@gmail.com>
2023-10-14 09:43:16 +0300

    set env variables

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-echo "$1"
-echo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
-echo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commits/$GITHUB_SHA"
-echo "$GITHUB_ACTOR_ID"
+. helpers.sh
 
+check_inputs $1 $2
+set_env $1 $2 $3
+
+echo $CHAT_ID
+echo $SLACK_TOKEN
+echo $MSG_TITLE
+echo $JOB_URL
+echo $COMMIT_URL
+echo $USER_AVATAR
 
